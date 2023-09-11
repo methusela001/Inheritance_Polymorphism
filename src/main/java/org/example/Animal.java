1be3cbd369813d51758132d56943367880682fb1
Methusela <nethusela777@gmail.com>
2023-09-11 22:09:59 +0200

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,20 @@
+package org.example;
+/*
+Inheritance - Polymorphism
+Exercise: Polymorphism
+Define a class Animal that has:
+
+an instance variable animalName
+a constructor that instantiate animalName
+a method animalSound() that prints default animal sound Define a subclass Lion Define a subclass Cow
+achieve polymorphism by using method overriding on animalSound(), printing the right sounds Define a tester class where you:
+create an Animal object
+create a Lion object
+create a Cow object
+executes the animalSound() method on all the 3 objects
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
